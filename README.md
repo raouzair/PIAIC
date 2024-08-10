5118f904a9c039ebcc35a60a1c4d2a6c823536bb
raouzair <raouzair@hotmail.com>
2024-08-11 02:08:26 +0500

    work done as on 110824
    11-08-24

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PIAIC
- 
+# This repository is for practice codes done by M Uzair Afzal studying PIAIC 62 AI and Cloud Computing at UMT Lahore.
---
+++ code.py
@@ -0,0 +1,22 @@
+# type: ignore
+
+
+input1 = 10 
+input2 = 5 
+
+input1 = input("Enter first numner: ")
+input2 = input("Enter second numner: ")
+
+ 
+
+x1 = input1 + input2
+print("The sum is ", x1)
+
+x2 = input1 - input2
+print("The Difference is ", x2)
+
+x3 = input1 * input2
+print("The multiple is ", x3)
+
+x4 = input1 / input2
+print("The division is ", x4)
