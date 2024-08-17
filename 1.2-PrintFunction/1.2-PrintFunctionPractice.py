e8c764138e0b02b153ced56f0f3ab86ddd0fd492
raouzair <raouzair@hotmail.com>
2024-08-17 21:44:18 +0500

    18082024-1

---
+++ 1.2-PrintFunction/1.2-PrintFunctionPractice.py
@@ -0,0 +1,28 @@
+print("Hello World Message.")
+print("Uzair")
+
+print("Uzair Rana")
+print("1 2 3")
+
+print("Hello \nWorld.")
+print("Hello \tWorld.")
+
+print("apple, banana, cherry.")
+print("1-2-3")
+
+print("Hello World.")
+print("12")
+
+print(" \"Hello World\" ")
+print("\\")
+
+print("I am 40 years old.")
+print("The number is 5")
+
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+
+print("\n1\n2\n3\n4\n5")
